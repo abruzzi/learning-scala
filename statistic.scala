6faa18a98cf34f8081efe2891f037269c0591018
Juntao Qiu <juntao.qiu@gmail.com>
2018-07-08 14:39:09 +0800

    info map

--- statistic.scala
+++ statistic.scala
@@ -1,5 +1,13 @@
 def infoMap(c: Char): Map[String, Double] = 
 	c match {
-		case 'd' => Map(("Dev day", 1.0))
+		case 'D' => Map(("In Dev", 1.0))
+		case 'd' => Map(("In Dev", 0.5))
+		case 'Q' => Map(("In QA", 1.0))
+		case 'q' => Map(("In QA", 0.5))
+		case 'B' => Map(("Blocked", 1.0))
+		case 'b' => Map(("Blocked", 0.5))			
+		case _ => Map(("Unknown", 0.0))
 	}
 
+
+"dDddD".toCharArray.map(infoMap).fold()
