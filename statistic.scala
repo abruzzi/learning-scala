bc4ad376a168d7faab19f6c60c5ce3df27dc1960
Juntao Qiu <juntao.qiu@gmail.com>
2018-07-08 15:33:46 +0800

    move code to Application

--- statistic.scala
+++ statistic.scala
@@ -1,19 +1,22 @@
-def convert(str: String): (String, Double) = 
-	str match {
-		case "D" => ("In Dev", 1.0)
-		case "d" => ("In Dev", 0.5)
-		case "Q" => ("In QA", 1.0)
-		case "q" => ("In QA", 0.5)
-		case "B" => ("Blocked", 1.0)
-		case "b" => ("Blocked", 0.5)		
-		case _ => ("Unknown", 0.0)
-	}
+object Application extends App {
+  def convert(str: String): (String, Double) = 
+    str match {
+        case "D" => ("In Dev", 1.0)
+        case "d" => ("In Dev", 0.5)
+        case "Q" => ("In QA", 1.0)
+        case "q" => ("In QA", 0.5)
+        case "B" => ("Blocked", 1.0)
+        case "b" => ("Blocked", 0.5)        
+        case _ => ("Unknown", 0.0)
+    }
 
-def calculate(input: String) = 
-	input.split("").map(convert).foldLeft(Map[String, Double]()) {
-		(map: Map[String, Double], status: (String, Double)) => 
-			val (key, value) = status
-			map + ((key, map.get(key).getOrElse(0.0) + value))
-	}
-
-calculate("ddDqQb")
+  def calculate(input: String) = 
+    input.split("").map(convert).foldLeft(Map[String, Double]()) {
+        (map: Map[String, Double], status: (String, Double)) => 
+            val (key, value) = status
+            map + ((key, map.get(key).getOrElse(0.0) + value))
+    }
+  
+  val result = calculate("ddDqQb")
+  println (result)
+}
