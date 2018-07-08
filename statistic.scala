68c6aaf1cf604618c8a64dc5c75d9bc6b0d777ee
Juntao Qiu <juntao.qiu@gmail.com>
2018-07-08 15:25:01 +0800

    fold

--- statistic.scala
+++ statistic.scala
@@ -1,13 +1,26 @@
-def infoMap(c: Char): Map[String, Double] = 
+def convert(c: Char): (String, Double) = 
 	c match {
-		case 'D' => Map(("In Dev", 1.0))
-		case 'd' => Map(("In Dev", 0.5))
-		case 'Q' => Map(("In QA", 1.0))
-		case 'q' => Map(("In QA", 0.5))
-		case 'B' => Map(("Blocked", 1.0))
-		case 'b' => Map(("Blocked", 0.5))			
-		case _ => Map(("Unknown", 0.0))
+		case 'D' => ("In Dev", 1.0)
+		case 'd' => ("In Dev", 0.5)
+		case 'Q' => ("In QA", 1.0)
+		case 'q' => ("In QA", 0.5)
+		case 'B' => ("Blocked", 1.0)
+		case 'b' => ("Blocked", 0.5)		
+		case _ => ("Unknown", 0.0)
 	}
 
+def reduce(list: List[(String, Double)])  =
+	list.foldLeft(Map[String, Double]()) {
+		(map: Map[String, Double], status: (String, Double)) => 
+			val (key, value) = status
+			map + ((key, map.get(key).getOrElse(0.0) + value))
+	}
 
-"dDddD".toCharArray.map(infoMap).fold()
+def calculate(input: String): Unit = 
+	val converted = input.toCharArray.map(convert)
+	converted.fold(Map[String, Double]()){
+		(map, status) => 
+		map + (status, map.get(status).getOrElse(0.0))
+	}
+
+calculate("ddDqQb")
