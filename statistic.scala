eec5a42c68f0c0d4328f9d30e288887aa3ee157c
Juntao Qiu <juntao.qiu@gmail.com>
2018-07-08 15:29:38 +0800

    refactor a little bit

--- statistic.scala
+++ statistic.scala
@@ -1,26 +1,19 @@
-def convert(c: Char): (String, Double) = 
-	c match {
-		case 'D' => ("In Dev", 1.0)
-		case 'd' => ("In Dev", 0.5)
-		case 'Q' => ("In QA", 1.0)
-		case 'q' => ("In QA", 0.5)
-		case 'B' => ("Blocked", 1.0)
-		case 'b' => ("Blocked", 0.5)		
+def convert(str: String): (String, Double) = 
+	str match {
+		case "D" => ("In Dev", 1.0)
+		case "d" => ("In Dev", 0.5)
+		case "Q" => ("In QA", 1.0)
+		case "q" => ("In QA", 0.5)
+		case "B" => ("Blocked", 1.0)
+		case "b" => ("Blocked", 0.5)		
 		case _ => ("Unknown", 0.0)
 	}
 
-def reduce(list: List[(String, Double)])  =
-	list.foldLeft(Map[String, Double]()) {
+def calculate(input: String) = 
+	input.split("").map(convert).foldLeft(Map[String, Double]()) {
 		(map: Map[String, Double], status: (String, Double)) => 
 			val (key, value) = status
 			map + ((key, map.get(key).getOrElse(0.0) + value))
-	}
-
-def calculate(input: String): Unit = 
-	val converted = input.toCharArray.map(convert)
-	converted.fold(Map[String, Double]()){
-		(map, status) => 
-		map + (status, map.get(status).getOrElse(0.0))
 	}
 
 calculate("ddDqQb")
